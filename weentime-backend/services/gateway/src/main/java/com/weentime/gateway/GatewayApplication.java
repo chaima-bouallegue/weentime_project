a2ffdecc32fd7f65f07e54d2b8deed1d1f0cb3af
Chaima Bouallegue <chaimabouallegue17@gmail.com>
2026-07-11 17:30:55 +0100

    test: trivial change to gateway to validate Docker Hub credentials fix in pipeline

--- weentime-backend/services/gateway/src/main/java/com/weentime/gateway/GatewayApplication.java
+++ weentime-backend/services/gateway/src/main/java/com/weentime/gateway/GatewayApplication.java
@@ -3,6 +3,7 @@ package com.weentime.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Test commit: validating Docker Hub credentials fix in pipeline
 @SpringBootApplication
 public class GatewayApplication {
 
